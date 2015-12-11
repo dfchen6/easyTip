9d75d2d9a34fce9370766ae763b0bcaa9d77eaf5
Difan Chen <difan@Difans-MacBook-Pro.local>
2015-12-11 02:12:53 -0600

    Built main view, working on setting page

--- easyTip/ViewController.swift
+++ easyTip/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipAmount: UILabel!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    var tipPercentages = [0.18, 0.2, 0.22]    // Default tips percentages
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func amountChanged(sender: AnyObject) {
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmounts = NSString(string: billAmount.text!).doubleValue
+        let tip = billAmounts * tipPercentage
+        let totalAmounts = billAmounts + tip
+        
+        tipAmount.text = String(format: "$%.2f", tip)
+        totalAmount.text = String(format: "$%.2f", totalAmounts)
+    }
 
 }
 
