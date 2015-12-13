48c3bb8d098627b734623bb278e393c0a43c9109
Difan Chen <difan@Difans-MacBook-Pro.local>
2015-12-13 01:13:32 -0600

    Local Currency, Night Mode Added

--- easyTip/SettingsViewController.swift
+++ easyTip/SettingsViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 protocol DestinationviewDelegate{
-    func setTipPerct(tipPerct1: Int, tipPerct2: Int, tipPerct3: Int);
+    func setTipPerct(tipPerct1: Int, tipPerct2: Int, tipPerct3: Int, isOn: Bool);
 }
 
 class SettingsViewController: UIViewController {
     var delegate : DestinationviewDelegate?
+
+    @IBOutlet weak var isNightMode: UISwitch!
     @IBOutlet weak var smallTipField: UITextField!
     @IBOutlet weak var midTipField: UITextField!
     @IBOutlet weak var largeTipField: UITextField!
     var p1: Int! = nil
     var p2: Int! = nil
     var p3: Int! = nil
+    var mode: Bool! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,18 +32,36 @@ class SettingsViewController: UIViewController {
         smallTipField.placeholder = "\(p1)"
         midTipField.placeholder = "\(p2)"
         largeTipField.placeholder = "\(p3)"
-
         // Do any additional setup after loading the view.
+        if(mode == true){
+            isNightMode.setOn(true, animated:true)
+        }
+        else{
+            isNightMode.setOn(false, animated:true)
+        }
+    }
+    
+    @IBAction func changeMode(sender: AnyObject) {
+        if(isNightMode.on){
+            print("On")
+            mode = true
+        }
+        else
+        {
+            print("Off")
+            mode = false
+        }
     }
     
     @IBAction func saveSettings(sender: AnyObject) {
         let tipPerc1 = Int(NSString(string: smallTipField.text!).doubleValue)
         let tipPerc2 = Int(NSString(string: midTipField.text!).doubleValue)
         let tipPerc3 = Int(NSString(string: largeTipField.text!).doubleValue)
-        print("keep working")
+        
         if((delegate) != nil){
             print("delegate works")
-            self.delegate?.setTipPerct(tipPerc1, tipPerct2: tipPerc2, tipPerct3: tipPerc3)
+            self.delegate?.setTipPerct(tipPerc1, tipPerct2: tipPerc2, tipPerct3: tipPerc3, isOn: mode)
+          
             self.navigationController?.popToRootViewControllerAnimated(true)
         }
     }
--- easyTip/ViewController.swift
+++ easyTip/ViewController.swift
@@ -10,17 +10,25 @@ import UIKit
 
 class ViewController: UIViewController, DestinationviewDelegate {
 
+    @IBOutlet weak var peopleTag: UILabel!
+    @IBOutlet weak var tipTag: UILabel!
+    @IBOutlet weak var totalTag: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var reduceButton: UIButton!
+    
     @IBOutlet weak var billAmount: UITextField!
     @IBOutlet weak var tipAmount: UILabel!
     @IBOutlet weak var totalAmount: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
-    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var numberField: UILabel!
     var tipPercentages = [0.18, 0.2, 0.22]    // Default tips percentages
     var numberOfPeople = 1 // Default number of people
+    var amount = 0.00      // Deafault number of money
+    var nightMode = false  // Default mode is day mode
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         billAmount.becomeFirstResponder()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,21 +39,24 @@ class ViewController: UIViewController, DestinationviewDelegate {
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+    
+    func updateNumView(num: Int){
+        self.numberOfPeople = num
+        numberField.text = "\(num)"
+    }
+    
     @IBAction func increasePeople(sender: AnyObject) {
-        var tmpNum = self.numberOfPeople
+        let tmpNum = self.numberOfPeople
         if( tmpNum < 10){
-            tmpNum = tmpNum + 1
-            self.numberOfPeople  = tmpNum
-            numberField.text = "\(tmpNum)"
+            updateNumView(tmpNum + 1)
         }
         amountChanged(self)
     }
+    
     @IBAction func decreasePeople(sender: AnyObject) {
-        var tmpNum = self.numberOfPeople
+        let tmpNum = self.numberOfPeople
         if( tmpNum > 1){
-            tmpNum = tmpNum - 1
-            self.numberOfPeople  = tmpNum
-            numberField.text = "\(tmpNum)"
+            updateNumView(tmpNum - 1)
         }
         amountChanged(self)
     }
@@ -56,21 +67,30 @@ class ViewController: UIViewController, DestinationviewDelegate {
         let tip = billAmounts * tipPercentage
         let totalAmounts = (billAmounts + tip)/Double(self.numberOfPeople)
         
-        tipAmount.text = String(format: "$%.2f", tip)
-        totalAmount.text = String(format: "$%.2f", totalAmounts)
+        tipAmount.text = formatAmount(tip)
+        //String(format: "$%.2f", tip)
+        print(formatAmount(tip))
+        totalAmount.text = formatAmount(totalAmounts)
     }
     
     // Called from the destination controller via delegate
-    func setTipPerct(tipPerct1: Int, tipPerct2: Int, tipPerct3: Int){
+    func setTipPerct(tipPerct1: Int, tipPerct2: Int, tipPerct3: Int, isOn: Bool){
         tipPercentages[0] = Double(Double(tipPerct1)/100);
         tipPercentages[1] = Double(Double(tipPerct2)/100);
         tipPercentages[2] = Double(Double(tipPerct3)/100);
         tipControl.setTitle("\(tipPerct1)%", forSegmentAtIndex: 0)
         tipControl.setTitle("\(tipPerct2)%", forSegmentAtIndex: 1)
         tipControl.setTitle("\(tipPerct3)%", forSegmentAtIndex: 2)
+        nightMode = isOn
         amountChanged(self)
-        
-        
+    }
+    
+    // Format the tip value and total value in local currency
+    func formatAmount (amount: Double) -> String{
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        formatter.locale = NSLocale.currentLocale() // This is the default
+        return formatter.stringFromNumber(amount)!
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -82,6 +102,57 @@ class ViewController: UIViewController, DestinationviewDelegate {
             destination.p1 = Int(tipPercentages[0]*100)
             destination.p2 = Int(tipPercentages[1]*100)
             destination.p3 = Int(tipPercentages[2]*100)
+            destination.mode = nightMode
+        }
+    }
+    
+    func switchMode(){
+        if(nightMode){
+            self.view.backgroundColor = UIColor(red:0.33, green:0.35, blue:0.57, alpha:0.5)
+            peopleTag.textColor = UIColor(white: 1, alpha: 1)
+            tipTag.textColor = UIColor(white: 1, alpha: 1)
+            totalTag.textColor = UIColor(white: 1, alpha: 1)
+            tipAmount.textColor = UIColor(white: 1, alpha: 1)
+            totalAmount.textColor = UIColor(white: 1, alpha: 1)
+            billAmount.textColor = UIColor(white: 1, alpha: 1)
+            billAmount.backgroundColor = UIColor(red:0.33, green:0.35, blue:0.57, alpha:0.5)
+            numberField.textColor = UIColor(white: 1, alpha: 1)
+            print("Night Mode is on! Let's work")
+        }
+        else{
+            peopleTag.textColor = UIColor.blackColor()
+            tipTag.textColor = UIColor.blackColor()
+            totalTag.textColor = UIColor.blackColor()
+            tipAmount.textColor = UIColor.blackColor()
+            totalAmount.textColor = UIColor.blackColor()
+            billAmount.textColor = UIColor.blackColor()
+            billAmount.backgroundColor = UIColor.whiteColor()
+            numberField.textColor = UIColor.blackColor()
+            print("Day Mode is on!")
+            self.view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    
+    func saveSettings(){
+        let previousDate = NSDate.timeIntervalSinceReferenceDate()
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(Int(NSString(string: billAmount.text!).doubleValue), forKey: "previousAmount")
+        defaults.setDouble(previousDate, forKey: "previousTime")
+        defaults.synchronize()
+    }
+    
+    func restoreSettings(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let prevAmount = defaults.integerForKey("previousAmount")
+                // Check re-enter time
+        let now = NSDate.timeIntervalSinceReferenceDate()
+        let prevTime = defaults.doubleForKey("previousTime")
+        print(now - prevTime)
+        if (prevAmount > 0 && (now - prevTime) < (10 * 60)){
+              billAmount.text = "\(prevAmount)"
+        }
+        else{
+            billAmount.text = ""
         }
     }
     
@@ -93,19 +164,9 @@ class ViewController: UIViewController, DestinationviewDelegate {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         print("view did appear")
-        let defaults = NSUserDefaults.standardUserDefaults()
-        let prevAmount = defaults.integerForKey("previousAmount")
-        
-        let now = NSDate.timeIntervalSinceReferenceDate()
-        let prevTime = defaults.doubleForKey("previousTime")
-        print(now - prevTime)
-        if (prevAmount > 0 && (now - prevTime) < 10*60){
-        billAmount.text = "\(prevAmount)"
+        restoreSettings()
+        switchMode()
         }
-        else{
-            billAmount.text = ""
-        }
-    }
     
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
@@ -115,13 +176,8 @@ class ViewController: UIViewController, DestinationviewDelegate {
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
         print("view did disappear")
-        let previousDate = NSDate.timeIntervalSinceReferenceDate()
-        //return (now - previousBillDate) < (10 * 60) // Ten minutes
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setInteger(Int(NSString(string: billAmount.text!).doubleValue), forKey: "previousAmount")
-        defaults.setDouble(previousDate, forKey: "previousTime")
-        defaults.synchronize()
-    }
+        saveSettings()
+        }
 
 
 }
